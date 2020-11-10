554e92efd7191c41e4f5ee8c693420eae6e2dde8
Kurt Choi <choi.kurt3@gmail.com>
2020-11-10 17:37:40 -0500

    playGeneration fix indentation to spaces

--- playGeneration.py
+++ playGeneration.py
@@ -9,55 +9,55 @@ DEBUG = False
 ROUND_DIGIT = 3
 
 def printWinRate(agentList, agentWinRate): # prints out agent winrate
-	if len(agentList) != len(agentWinRate):
-		print("size of agentList and agentWinRate do not match")
-		return
+    if len(agentList) != len(agentWinRate):
+        print("size of agentList and agentWinRate do not match")
+        return
 
-	for i in range(len(agentList)):
-		print(agentList[i] + " winrate", end=': ')
-		print(str(round(agentWinRate[i] * 100, ROUND_DIGIT)) + "%")
+    for i in range(len(agentList)):
+        print(agentList[i] + " winrate", end=': ')
+        print(str(round(agentWinRate[i] * 100, ROUND_DIGIT)) + "%")
 
 # main ---------------------------
 
 def main():
 
-	with open("agentList") as f:
-		agentList = f.read().splitlines() # read agent filenames from "/agentList"
+    with open("agentList") as f:
+        agentList = f.read().splitlines() # read agent filenames from "/agentList"
 
-	agentWinCount = np.zeros(len(agentList)) # init wincount array
-	agentPlayCount = np.zeros(len(agentList)) # init playcount array
-	agentWinRate = np.zeros(len(agentList)) # init winrate array
+    agentWinCount = np.zeros(len(agentList)) # init wincount array
+    agentPlayCount = np.zeros(len(agentList)) # init playcount array
+    agentWinRate = np.zeros(len(agentList)) # init winrate array
 
-	for indexA in range(len(agentList) - 1): # iterate
-		for indexB in range(indexA, len(agentList)):
-			# open gene files
-			with open(AGENT_DIR + agentList[indexA]) as f:
-				genesA = f.read().splitlines()
-			with open(AGENT_DIR + agentList[indexB]) as f:
-				genesB = f.read().splitlines()
+    for indexA in range(len(agentList) - 1): # iterate
+        for indexB in range(indexA, len(agentList)):
+            # open gene files
+            with open(AGENT_DIR + agentList[indexA]) as f:
+                genesA = f.read().splitlines()
+            with open(AGENT_DIR + agentList[indexB]) as f:
+                genesB = f.read().splitlines()
 
-			for x in range(NUM_GAMES_PER_PAIR): # play x games
-				# set player colour
-				if x % 2 == 0:
-					colourA, colourB = 'w', 'b'
-				else:
-					colourA, colourB = 'b', 'w'
+            for x in range(NUM_GAMES_PER_PAIR): # play x games
+                # set player colour
+                if x % 2 == 0:
+                    colourA, colourB = 'w', 'b'
+                else:
+                    colourA, colourB = 'b', 'w'
 
-				blackWon = playGame(colourA, genesA, colourB, genesB, 1, DEBUG)
-				agentPlayCount[indexA] += 1
-				agentPlayCount[indexB] += 1
+                blackWon = playGame(colourA, genesA, colourB, genesB, 1, DEBUG)
+                agentPlayCount[indexA] += 1
+                agentPlayCount[indexB] += 1
 
-				if(colourA == 'b'): 
-					agentWinCount[indexA] += blackWon
-					agentWinCount[indexB] += (1 - blackWon)
-				else:
-					agentWinCount[indexB] += blackWon
-					agentWinCount[indexA] += (1 - blackWon)
+                if(colourA == 'b'): 
+                    agentWinCount[indexA] += blackWon
+                    agentWinCount[indexB] += (1 - blackWon)
+                else:
+                    agentWinCount[indexB] += blackWon
+                    agentWinCount[indexA] += (1 - blackWon)
 
-	for i in range(len(agentList)):
-		agentWinRate[i] = agentWinCount[i] / agentPlayCount[i]
+    for i in range(len(agentList)):
+        agentWinRate[i] = agentWinCount[i] / agentPlayCount[i]
 
-	printWinRate(agentList, agentWinRate)
+    printWinRate(agentList, agentWinRate)
 
 if __name__ == "__main__":
     main()
